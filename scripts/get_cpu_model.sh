5336ed90eee231d87c993e7b18db26b679fd03a2
Charlie Hsieh <charlieofficial@gmail.com>
2019-11-03 03:37:34 +0800

    fix for multi-core cpu

--- scripts/get_cpu_model.sh
+++ scripts/get_cpu_model.sh
@@ -10,6 +10,7 @@
 #-----------------------
 
 function get_cpu_model(){
-  local this_cpu_name="$(cat /proc/cpuinfo |grep -Eo 'CPU[[:print:]]+@ [[:digit:]|.]+GHz')"
-  echo "${this_cpu_name}"
+  local this_cpu_name="$(cat /proc/cpuinfo | grep 'model name' | grep -Eo 'CPU[[:print:]]+@ [[:digit:]|.]+GHz' | head -1)"
+  local this_cpu_count="$(cat /proc/cpuinfo | grep 'model name' | wc -l)"
+  echo "${this_cpu_name} ->${this_cpu_count"
 }
