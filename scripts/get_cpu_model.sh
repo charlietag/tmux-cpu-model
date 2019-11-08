7785aa1f365e30cc1fb0cbb7374d7d2e1b2a2412
Charlie Hsieh <charlieofficial@gmail.com>
2019-11-08 14:16:13 +0800

    remove comments

--- scripts/get_cpu_model.sh
+++ scripts/get_cpu_model.sh
@@ -1,14 +1,3 @@
-#!/usr/bin/env bash
-
-# sample:
-## displaying variable content, line by line
-#for plugin in $plugins_list; do
-#    echo $plugin
-#done
-
-
-#-----------------------
-
 function get_cpu_model(){
   local this_cpu_name="$(cat /proc/cpuinfo | grep 'model name' | grep -Eo 'CPU[[:print:]]+@ [[:digit:]|.]+GHz' | head -1)"
   local this_cpu_count="$(cat /proc/cpuinfo | grep 'model name' | wc -l)"
